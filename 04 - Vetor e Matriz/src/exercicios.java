a54dca4bcb01ef912840d793f6c5d94f34d134ac
Bangbang123123 <romulo.gamedesigner@gmail.com>
2025-08-21 17:40:40 -0300

    04 // 8 - 15 completo

--- 04 - Vetor e Matriz/src/exercicios.java
+++ 04 - Vetor e Matriz/src/exercicios.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class exercicios {
 	public static void main(String[] args) {
@@ -142,31 +143,200 @@ public class exercicios {
 			
 			break;
 		case 8:
+			final String vog = "aeiou";
+			final String con = "bcdfghjklmnpqrstvwxyz";
+			int senha_tam;
+			do {
+				System.out.print("Informe o tamanho da senha: ");
+				senha_tam = input.nextInt();
+				if (senha_tam > 26) {
+					System.out.print("Maior que 26, tente de novo\n");
+				}
+			} while (senha_tam > 26);
+			
+			Random rand = new Random();
+			System.out.print("Senha: ");
+			for (int i = 0; i < senha_tam; i++) {
+				if (i % 2 == 0) {
+					System.out.print(con.charAt(rand.nextInt(26)));
+				} else {
+					System.out.print(vog.charAt(rand.nextInt(5)));
+				}
+			}
 			
 			break;
 		case 9:
+			System.out.print("Informe um texto: ");
+			input.nextLine();
+			String text = input.nextLine();
+			char[] text2 = new char[text.length()];
+			for (int i = 0; i < text.length(); i++) {
+				text2[i] = text.charAt(i);
+				System.out.print("["+ text2[i] +"]");
+			}
 			
 			break;
 		case 10:
-			
+			System.out.print("Informe uma data (dd/mm/aaaa): ");
+			input.nextLine();
+			String[] data = input.nextLine().split("/");
+			int[] data_int = new int[3];
+			if (data.length == 3) {
+				for (int i = 0; i < 3; i++) {
+					data_int[i] = Integer.parseInt(data[i]);
+				}
+				System.out.print("Data: "+ data_int[0] +"/"+ data_int[1] +"/"+ data_int[2]);
+			} else {
+				System.out.print("Data inválida");
+			}
 			break;
 		case 11:
-			
+			System.out.print("Informe uma lista de nomes separadas por ';' : ");
+			input.nextLine();
+			String[] nomes = input.nextLine().split(";");
+			System.out.print("Nomes: ");
+			for (int i = 0; i < nomes.length; i++) {
+				System.out.print("["+ nomes[i] +"]");
+			}
+				
 			break;
 		case 12:
+			float[] alt = new float[5];
+			int[] idade = new int[5];
+			
+			int alto = 0;
+			int baixo = 0;
+			int velho = 0;
+			int novo = 0;
+			float med_idade = 0;
+			float med_alt = 0;
+			
+			for (int i = 0; i < 5; i++) {
+				System.out.println("Informe a altura e idade da "+ (i+1) +"° pessoa: ");
+				alt[i] = input.nextFloat();
+				idade[i] = input.nextInt();
+				if (alt[i] > alt[alto]) {
+					alto = i;
+				}
+				if (alt[i] < alt[baixo]) {
+					baixo = i;
+				}
+				if (idade[i] > idade[velho]) {
+					velho = i;
+				}
+				if (idade[i] < idade[novo]) {
+					novo = i;
+				}
+				med_idade += idade[i];
+				med_alt += alt[i];
+			}
+			med_alt /= 5;
+			med_idade /= 5;
+			System.out.print("\nMédia de altura: "+ med_alt
+							+"\nMédia de idade: "+ med_idade
+							+"\nMais alto: Altura: "+ alt[alto] +" // Idade: "+ idade[alto]
+							+"\nMais baixo: Altura: "+ alt[baixo] +" // Idade: "+ idade[baixo]
+							+"\nMais velho: Altura: "+ alt[velho] +" // Idade: "+ idade[velho]
+							+"\nMais novo: Altura: "+ alt[novo] +" // Idade: "+ idade[novo]
+							+"\nVetor: ");
+			for (int i = 0; i < 5; i++) {
+				System.out.print("[Altura: "+ alt[i] +"// Idade: "+ idade[i] +"] ");
+			}
+			System.out.print("\nAcima da média (altura): ");
+			for (int i = 0; i < 5; i++) {
+				if (alt[i] > med_alt)
+					System.out.print("[Altura: "+ alt[i] +"// Idade: "+ idade[i] +"] ");
+			}
+			System.out.print("\nAbaixo da média (altura): ");
+			for (int i = 0; i < 5; i++) {
+				if (alt[i] < med_alt)
+					System.out.print("[Altura: "+ alt[i] +"// Idade: "+ idade[i] +"] ");
+			}
+			System.out.print("\nAcima da média (idade): ");
+			for (int i = 0; i < 5; i++) {
+				if (idade[i] > med_idade)
+					System.out.print("[Altura: "+ alt[i] +"// Idade: "+ idade[i] +"] ");
+			}
+			System.out.print("\nAbaixo da média (idade): ");
+			for (int i = 0; i < 5; i++) {
+				if (idade[i] < med_idade)
+					System.out.print("[Altura: "+ alt[i] +"// Idade: "+ idade[i] +"] ");
+			}
 			
 			break;
 		case 13:
+			int[] v1 = new int[10];
+			int[] v2 = new int[10];
+			int[] v3 = new int[20];
+			
+			System.out.print("Informe os 20 números: ");
+			for (int i = 0; i < 20; i++) {
+				if (i <= 9) {
+					v1[i] = input.nextInt();
+				} else {
+					v2[i-10] = input.nextInt();
+				}
+			}
+			System.out.print("Vetor intercalado: ");
+			for (int i = 0; i < 20; i++) {
+				if (i % 2 == 0) {
+					v3[i] = v1[i/2];
+				} else {
+					v3[i] = v2[(i-1)/2];
+				}
+				System.out.print("["+ v3[i] +"]");
+			}
 			
 			break;
 		case 14:
+			float[] altura = new float[30];
+			int[] id = new int[30];
+
+			float med_id = 0;
+			float med_altura = 0;
 			
+			for (int i = 0; i < 30; i++) {
+				System.out.println("Informe a altura e idade da "+ (i+1) +"° pessoa: ");
+				altura[i] = input.nextFloat();
+				id[i] = input.nextInt();
+				med_id += id[i];
+				med_altura += altura[i];
+			}
+			med_altura /= 30;
+			med_id /= 30;
+			System.out.print("\nMédia de altura: "+ med_altura
+							+"\nMédia de idade: "+ med_id
+							+"\nMaiores de 13 anos com altura abaixo da média:\n");
+			for (int i = 0; i < 30; i++) {
+				if (id[i] > 13 && altura[i] < med_altura) {
+					System.out.print("[Altura: "+ altura[i] +"// Idade: "+ id[i] +"] ");
+				}
+			}
 			break;
 		case 15:
+			final int N = 5;
+			int[] vetor = new int[N];
+			
+			int i = 0;
+			while (i < N) {
+				System.out.print("Informe o "+ (i+1) +"° número [0-20]: ");
+				vetor[i] = input.nextInt();
+				if (vetor[i] < 0 || vetor[i] > 20) {
+					System.out.print("\nInválido, somente entre [0-20]\n\n");
+				} else {
+					i++;
+				}
+			}
+			
+			System.out.print("Gráfico:");
+			for (int j = 0; j < N; j++) {
+				System.out.print("\n"+ j +" : ");
+				for (int h = 0; h < vetor[j]; h++) {
+					System.out.print("#");
+				}
+			}
 			
 			break;
-
-			
 		}
 		input.close();
 	}
