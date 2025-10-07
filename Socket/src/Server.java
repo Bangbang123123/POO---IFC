29a5939783316b3382507c824587f14661fc632c
Bangbang123123 <romulo.gamedesigner@gmail.com>
2025-10-07 19:50:48 -0300

    Desafio 04 - concluído

---
+++ Socket/src/Server.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Server {
+	
+	public static void main(String[] args) throws IOException {
+		
+		ServerSocket servidor = null;
+		Socket conexao = null;
+		BufferedReader entrada = null;
+		List<Evento> eventos = new ArrayList<>();
+		
+		try {
+			servidor = new ServerSocket(1306);
+			conexao = servidor.accept();
+			entrada = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+			PrintStream saida = new PrintStream(conexao.getOutputStream());
+			do {
+				
+				String texto = entrada.readLine();
+				if (texto.equals("data")) {
+					
+					texto = entrada.readLine();
+					String resposta = "";
+					for (int i = 0; i < eventos.size(); i++) {
+						if (texto.equals(eventos.get(i).getFuso())) {
+							resposta += eventos.get(i).toString() + ";";
+						}
+					}
+					saida.println(resposta);
+					
+				} else {
+					
+					Evento evento = new Evento(texto);
+					boolean verificar = true;
+					
+					for (int i = 0; i < eventos.size(); i++) {
+						if (eventos.get(i).verificaEvento(evento)) {
+							verificar = false;	
+							break;
+						}
+					}
+					
+					if (verificar) {
+						eventos.add(evento);
+					}
+					
+					for (int i = 0; i < eventos.size(); i++) {
+						System.out.println(eventos.get(i).toString());
+						
+					}
+				}
+			} while(true);
+
+		} catch(IOException e) {
+			System.out.println("Algo errado aconteceu");
+		} finally {
+			conexao.close();
+			servidor.close();
+		}
+	}
+}
